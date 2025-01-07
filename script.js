fa956b60ad1957a7124a6e08ef39b23f606db037
xYulania <xyulania@gmail.com>
2025-01-08 00:56:15 +0300

    initial commit

--- script.js
+++ script.js
@@ -5,4 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     calendar.render();
 });
-  
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        dateClick: function(info) {
+            alert('Clicked on: ' + info.dateStr);
+            alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+            alert('Current view: ' + info.view.type);
+            // change the day's background color just for fun
+            info.dayEl.style.backgroundColor = 'red';
+          }
+    });
+    calendar.render();
+});
